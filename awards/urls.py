13a885dabf4045ea600709000e09dca183cb39fd
nivlapeter <nivlapeter.0549@gmail.com>
2020-10-28 03:59:46 +0300

    add url in the awards/urls.py file

--- awards/urls.py
+++ awards/urls.py
@@ -1,5 +1,26 @@
+"""accolades URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.conf.urls import url
 
 urlpatterns = [
-    
+    url('', views.index, name = 'index')
+    url('myprofile/', views.profile, name = 'myprofile')
+    url('profile/edit/', views.edit_profile, name = 'edit')
+    url('profile/upload', views.upload_profile, name = 'upload')
+    url('project/<project_id>/', views.project, name = 'project')
+    url('search/', views.search, name='search')
+
 ]
