167000b23dc09b955c03480e85fc4b80361e9886
nivlapeter <nivlapeter.0549@gmail.com>
2020-10-28 04:02:25 +0300

    Register the Models

--- awards/admin.py
+++ awards/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+admin.site.register(Project)
+admin.site.register(Profile)
+admin.site.register(Ratings)
+admin.site.register(Categories)
