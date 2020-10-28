8a890ef3d7f672d2e01549db70d38fcdba9284be
nivlapeter <nivlapeter.0549@gmail.com>
2020-10-28 03:45:06 +0300

    add index view

--- awards/views.py
+++ awards/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Project,Profile,Ratings,Categories
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required(login_url='/accounts/login/')
+def index(request):
+    current_user = request.user
+    projects = Project.objects.order_by('pub_date').all()
+
+    return render(request,'index.html',{"projects":projects})
